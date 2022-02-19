d6054d22d013c1bdaf22b8eb1c5210b25bb46244
agiklo <54153910+agiklo@users.noreply.github.com>
2022-02-19 22:20:18 +0100

    feat: create feign clients service

--- clients/src/main/java/pl/mmilewczyk/clients/notification/NotificationClient.java
+++ clients/src/main/java/pl/mmilewczyk/clients/notification/NotificationClient.java
@@ -3,7 +3,7 @@ package pl.mmilewczyk.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient("NOTIFICATION")
 public interface NotificationClient {
 
     @PostMapping("api/v1/notification")
--- clients/src/main/java/pl/mmilewczyk/clients/user/UserClient.java
+++ clients/src/main/java/pl/mmilewczyk/clients/user/UserClient.java
@@ -3,10 +3,14 @@ package pl.mmilewczyk.clients.user;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("user-service")
+@FeignClient("USER-SERVICE")
 public interface UserClient {
 
     @GetMapping("api/v1/users/loggedInUser")
     ResponseEntity<UserResponseWithId> getLoggedInUser();
+
+    @GetMapping("api/v1/users/{username}")
+    ResponseEntity<UserResponseWithId> getUserByUsername(@PathVariable("username") String username);
 }
