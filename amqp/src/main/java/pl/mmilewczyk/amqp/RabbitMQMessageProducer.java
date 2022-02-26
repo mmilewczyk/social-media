87295dff3e373cf1a5e134a110aad51bff34639d
agiklo <54153910+agiklo@users.noreply.github.com>
2022-02-26 21:51:26 +0100

    feat: create amqp module which contains rabbitmq configuration and publish method

---
+++ amqp/src/main/java/pl/mmilewczyk/amqp/RabbitMQMessageProducer.java
@@ -0,0 +1,20 @@
+package pl.mmilewczyk.amqp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQMessageProducer {
+
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object payload, String exchange, String routingKey) {
+        log.info("Publishing to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
+        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+        log.info("Published to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
+    }
+}
